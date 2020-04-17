0cc58524cc9a631efb3f6cea6e2e83fc216bd4fa
Manu <mcorcos@itba.edu.ar>
2020-04-17 16:38:07 -0300

    ORDENO CLIENT

--- TP5-EDA-CLIENT/client.cpp
+++ TP5-EDA-CLIENT/client.cpp
@@ -2,6 +2,10 @@
 
 size_t write_callback(char* contents, size_t size, size_t numbermemb, void* userData);
 
+
+/*     CONTRUCTOR   */
+
+
 Client::Client()
 {
 	 setErr(curl_global_init(CURL_GLOBAL_ALL));
@@ -19,20 +23,39 @@ Client::Client()
 	}
 }
 
+
+/*     DESTRUCTOR   */
+
 Client::~Client()
 {
 	curl_easy_cleanup(curl_handler);
 }
 
-void Client::setErr(CURLcode err) { error = err; };    //setter
 
-CURLcode Client::getErr() { return error; };    //getter 
+
+/*     SETTER   */
+
+
+
+void Client::setErr(CURLcode err) { error = err; };   
+
+
+/*     GETTER   */ 
+
+
+CURLcode Client::getErr() { return error; };  
 CURL* Client::getCurlhand() { return curl_handler; };
+
+
+
+/*     FUNCIONES MIEMBRO   */
+
+
 
 
 void Client::curl_initial_set()
 {
-	curl_easy_setopt(curl_handler, CURLOPT_URL, "localhost");			//NO ENTIENDO QUE SERIA LOCAL HOST tipo Host: 127.0.0.1 CRLF ... CRLF ?
+	curl_easy_setopt(curl_handler, CURLOPT_URL, "25.135.151.200");			//NO ENTIENDO QUE SERIA LOCAL HOST tipo Host: 127.0.0.1 CRLF ... CRLF ?
 	curl_easy_setopt(curl_handler, CURLOPT_PORT, 13);				//Escuchamos puerto 80
 	curl_easy_setopt(curl_handler, CURLOPT_VERBOSE, 1L);
 	curl_easy_setopt(curl_handler, CURLOPT_PROTOCOLS,CURLPROTO_HTTP);
@@ -40,7 +63,13 @@ void Client::curl_initial_set()
 	curl_easy_setopt(curl_handler, CURLOPT_WRITEDATA, (void*)&userData_);	//Paso la data guardada por callback en this->userData a donde???
 }
 
+
+
+
 /*COMENTARIO  --> copie la del ej getinmemory.c pero desps podemos armar una buena con <fstream>*/
+
+
+
 size_t write_callback(char* contents, size_t size, size_t cantmemb, void* CurrentUserData)
 {
 	size_t realsize = size * cantmemb;
